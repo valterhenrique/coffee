e0d730af7927bf8150ddd8fb9e4f729b9f0e525c
Valter Henrique <valterhenrique85@gmail.com>
2016-12-28 20:16:15 +0100

    Create a parser from csv to java bean

---
+++ src/com/valterhenrique/parser/Employee.java
@@ -0,0 +1,41 @@
+package com.valterhenrique.parser;
+
+public class Employee {
+
+    private String empId;
+    private String firstName;
+    public String lastName;
+    public String salary;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
